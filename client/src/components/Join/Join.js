06f358a04d2c0b9820fbc78b831c37d0777e3ec7
Edward Chau <edwardchau@berkeley.edu>
2020-08-15 22:32:25 -0700

    remove ds_store

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -10,7 +10,7 @@ const Join = () => {
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
+                <h1 className="heading">poker.io</h1>
                 <div>
                     <input placeholder="Name" className="joinInput" type="text" 
                         onChange={(event) => setName(event.target.value)} />
