3966e67099222b012d9498f08b035eef2ae330ef
Edward Chau <edwardchau@berkeley.edu>
2020-08-15 21:01:02 -0700

    rooms

---
+++ client/src/components/TextContainer/TextContainer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './TextContainer.css'
+
+const TextContainer = ({ users }) => (
+    <div className="textContainer">
+        { 
+            users ? (
+                <div>
+                    <h1>Players:</h1>
+                    <div className="activeContainer">
+                        <h2>
+                            {users.map(({name}) => (
+                            <div key={name} className="activeItem">
+                                {name}
+                                <img alt="online" src={onlineIcon}/>
+                            </div>
+                            ))}
+                        </h2>
+                    </div>
+                </div>
+            ) : null 
+        }
+    </div>
+)
+
+export default TextContainer
---
+++ server/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js')
+
+const PORT = process.env.PORT || 5000
+
+const router = require('./router')
+const users = require('./users.js')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(router)
+
+io.on('connection', (socket) => {
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room })
+
+        if(error) return callback(error)
+
+        socket.emit('message', {user: 'admin', text: `${user.name}, welcome to ${user.room}`})
+        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name} has joined.`})
+
+        socket.join(user.room)
+
+        io.to(user.room).emit('roomData', {room:user.room, users:getUsersInRoom(user.room)})
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+
+        io.to(user.room).emit('message', {user: user.name, text: message})
+
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if(user) {
+            io.to(user.room).emit('message', {user:'admin', text:`${user.name} has left.`})
+        }
+    })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server has start on port ${PORT}`)
+})
